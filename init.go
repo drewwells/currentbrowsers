9335d4e8b74142853739b2c80fdde3639718bcb0
Drew Wells <drew.wells00@gmail.com>
2014-06-30 12:11:10 -0500

    Apply CORS headers

--- init.go
+++ init.go
@@ -25,9 +25,21 @@ type Browser struct {
 
 func init() {
 	r := mux.NewRouter()
-	r.HandleFunc("/", IndexHandler)
-	r.HandleFunc("/check", CheckHandler)
+	r.HandleFunc("/", addDefaultHeaders(IndexHandler))
+	r.HandleFunc("/check", addDefaultHeaders(CheckHandler))
 	http.Handle("/", r)
+}
+
+func addDefaultHeaders(fn http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if origin := r.Header.Get("Origin"); origin != "" {
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+		}
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+		fn(w, r)
+	}
 }
 
 // IndexHandler is responsible for listing the most
